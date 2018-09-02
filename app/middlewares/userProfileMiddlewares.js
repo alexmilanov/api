32e5a51729a00da268772dbb4325ec62e8747e1c
Alexander Milanov <aleksandar.milanov@opencode.com>
2018-09-02 20:07:35 +0300

    Add updateEmail and updatePassword middlewares

--- app/middlewares/userProfileMiddlewares.js
+++ app/middlewares/userProfileMiddlewares.js
@@ -5,28 +5,45 @@ var User = require('../../app/models/user');
 
 module.exports = {
     getAccountData: function(req, res) {
-        if(!req.query.username) res.json({
-            success: false,
-            error: "Username is mandatory"
-        })
+        if(!req.query.username) {
+                return res.json({
+                success: false,
+                error: "Username is mandatory"
+            })
+        }
 
-            User.findOne({username: req.query.username}, (err, users)=> {
-                if(err) throw err;
+        User.findOne({username: req.query.username}, (err, users)=> {
+            if(err) throw err;
 
-                if(users) {	
-                    return res.json({
-                        success: true,
-                        lastLogin: users.userAccountData.lastLogin,
-                        accountCreatedAt: users.userAccountData.createdAt,
-                        username: users.username,
-                        email: users.email
-                    });
-                }
+            if(users) {	
+                return res.json({
+                    success: true,
+                    lastLogin: users.userAccountData.lastLogin,
+                    accountCreatedAt: users.userAccountData.createdAt,
+                    username: users.username,
+                    email: users.email
+                });
+            }
 
             return res.json({
                 success: false,
                 error: "No such user"
             });
         })
-    }
+    },
+
+    updateEmail: function(req, res) {
+        if(!req.query.password) {
+            res.json({
+                success: false,
+                error: "Email and/or Username is mandatory"
+            })
+        }
+
+        User.findOneAndUpdate()
+    },
+
+    updatePassword: function(req, res) {
+
+    },
 }
